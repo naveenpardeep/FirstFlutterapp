214c16130d58faaaf7f627c9a7635983d4a2f3ee
naveenpardeep <naveensheoran1000@gmail.com>
2021-11-04 17:10:25 +0300

    day 3 complete

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,7 +1,5 @@
 
 
-import 'dart:html';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -9,7 +7,9 @@ import 'package:flutter/rendering.dart';
 class Login extends StatelessWidget  {
   @override
   Widget build(BuildContext context) {
-
+    // to get system width height
+    double width= MediaQuery.of(context).size.width;
+    double height=MediaQuery.of(context).size.height;
     return Scaffold(
 
       body: Center (
@@ -21,22 +21,54 @@ class Login extends StatelessWidget  {
 
 
         children:  [
+           Container(
+             width: width*0.5,
+             height: height*0.5,
+             child: Column(
+               children: [
+
+                 //Image.asset("assets/images/login_pic.png" ),
+                 //  Image(image: AssetImage('assets/images/login_pic.png')),
+                 Text("Welcome",textScaleFactor: 3) ,
+                    SizedBox(height: 10,),
+                 TextFormField(textAlign: TextAlign.center,
+                   decoration: InputDecoration(border: const OutlineInputBorder(
+                     borderRadius: BorderRadius.all(Radius.circular(20))),
+                      hintText: "Enter your login id", labelText: "Login id" ),),
+
+                     SizedBox(height: 10,),
+                  TextFormField(textAlign: TextAlign.center, obscureText: true,
+                   decoration: InputDecoration(border: const OutlineInputBorder(
+                     borderRadius: BorderRadius.all(Radius.circular(20))
+                   ),hintText: "Enter your password", labelText: "Password"),
+                 ),
+                    SizedBox(height: 20,),
+
+                    Container(
+                      width: width*0.5  ,
+                      height: height*0.08,
+                      child : 
+                            ElevatedButton(
+                         
+                        style: ButtonStyle(
+                          
+                          shape:MaterialStateProperty.all( RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))) ,
+                          backgroundColor: MaterialStateProperty.all( Colors.lightBlue[50]),
+                          foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                       ),
+                           onPressed: () {
+                             //on click functionality here
+                            },
+                             child: Text('Login',textScaleFactor: 2),
+                       ),
+                      ),
+
+               ],
+             ),
+           )
 
 
-          //Image.asset("assets/images/login_pic.png" ),
-        //  Image(image: AssetImage('assets/images/login_pic.png')),
-          Text("Welcome",textScaleFactor: 3) ,
-        Padding(
-          padding: EdgeInsets.fromLTRB(540,20,540,10),
-         child: TextFormField(textAlign: TextAlign.center,
-           decoration: InputDecoration(border: const OutlineInputBorder(),hintText: "Enter User Name", labelText: "User Name" ),),
-        ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(540, 10, 540, 20),
-          child: TextFormField(textAlign: TextAlign.center, obscureText: true,
-          decoration: InputDecoration(border: const OutlineInputBorder(),hintText: "Enter password", labelText: "Paasword"),
-          ),
-          ),
 
 
         ],
